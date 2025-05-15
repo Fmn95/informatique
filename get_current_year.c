46ce187cbd6f2321281bf8da8cd0843358db9458
Fmn95 <firmin.godinou@gmail.com>
2025-05-15 10:13:55 +0200

    Update get_current_year.c

--- get_current_year.c
+++ get_current_year.c
@@ -1,6 +1,6 @@
 #include "includes.h"
 //Calcule l annee actuelle
-int get_current_year() {
+int annee_courante() {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
     return tm.tm_year + 1900;
